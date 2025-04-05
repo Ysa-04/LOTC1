82c2841be82623f15b9a3816553707288140a9cb
Ysa-04 <ysaura2903@outlook.com>
2025-04-05 14:45:16 +0000

    types.ts aangemaakt + UserAccount interface aangemaakt

--- index.ts
+++ index.ts
@@ -42,3 +42,15 @@ import { UserAccount } from "./types";
 
 const CONNECTION_STRING: string = "mongodb+srv://ysaura_va:d2pr0v6BMxfqh6cx@school.qkqtytv.mongodb.net/?retryWrites=true&w=majority&appName=School";
 const client: MongoClient = new MongoClient(CONNECTION_STRING);
+
+async function main(){
+  try {
+
+  }
+  catch (e) {
+    console.error(e);
+  }
+  finally {
+    await client.close();
+  }
+}main();
