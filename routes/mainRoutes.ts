e1ff6c2c2956c063d25fd0db8e44bf3d0318d40b
Ysa-04 <ysaura2903@outlook.com>
2025-04-16 20:01:23 +0000

    logischere route file aangemaakt + alle routes in 1 file gestoken, dit is handiger voor wanneer we met accounts gaan werken

---
+++ routes/mainRoutes.ts
@@ -0,0 +1,45 @@
+import express, {Router} from "express";
+import { appendFile } from "fs";
+const router:Router = express.Router();
+
+router.get("/", (req, res) => {
+    res.render('index');
+});
+router.get("/10-rounds", (req, res) => {
+    res.render("10-rounds");
+});
+router.get("/404landingpage", (req, res) => {
+    res.render("404landingpage");
+});
+router.get("/404", (req, res) => {
+    res.render("404");
+});
+router.get("/blacklist", (req, res) => {
+    res.render("blacklist");
+});
+router.get("/favourite", (req, res) => {
+    res.render("favorite");
+});
+router.get("/highscore", (req, res) => {
+    res.render("highscore");
+});
+router.get("/homepage", (req, res) => {
+    res.render("homepage");
+});
+router.get("/login", (req, res) => {
+    res.render("login");
+});
+router.get("/mistakes", (req, res) => {
+    res.render("mistakes");
+});
+router.get("/register", (req, res) => {
+    res.render("register");
+});
+router.get("/result", (req, res) => {
+    res.render("result");
+});
+/* router.get("/sudden-death", (req, res) => {
+    res.render("suddend-death");
+}); */
+
+export default router;
