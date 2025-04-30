c73984682715ee56f15a694426d19d841e901e6a
Ysa-04 <ysaura2903@outlook.com>
2025-04-30 12:32:33 +0000

    data.ts

--- index.ts
+++ index.ts
@@ -16,23 +16,4 @@ app.listen(app.get("port"), () =>
   console.log("[server] http://localhost:" + app.get("port"))
 );
 
-//database (mongodb)
-
-import { Collection, MongoClient } from "mongodb";
-import { UserAccount } from "./types";
-
-const CONNECTION_STRING: string = "mongodb+srv://ysaura_va:d2pr0v6BMxfqh6cx@school.qkqtytv.mongodb.net/?retryWrites=true&w=majority&appName=School";
-const client: MongoClient = new MongoClient(CONNECTION_STRING);
-
-async function main(){
-  try {
-
-  }
-  catch (e) {
-    console.error(e);
-  }
-  finally {
-    await client.close();
-  }
-}main();
 
