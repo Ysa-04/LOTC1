8d47da46fb9c0aad39acd05ebd6ab1e4d71d0065
Britt van de Reijt <britt.1412vandereijt@gmail.com>
2025-05-24 12:11:27 +0000

    app.ts

--- app.ts
+++ app.ts
@@ -5,19 +5,40 @@ import dotenv from 'dotenv';
 import path from 'path';
 
 //laten staan
+const express = require('express');
+const path = require('path');
+const dotenv = require('dotenv');
+
 dotenv.config();
+
+const authRoutes = require('./routes/auth');
+const quizRoutes = require('./routes/quiz');
+const favoriteRoutes = require('./routes/favorites');
+const blacklistRoutes = require('./routes/blacklist');
+const { connectToDb } = require('./models/db');
+
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-//middlewares: laten staan
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
-app.use(express.static(path.join(__dirname, 'public')));
+//middlewares:
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 
 //hiertussen de rest typen :)
+connectToDb().then(() => console.log('Connected to MongoDB'));
+
+app.use('/auth', authRoutes);
+app.use('/quiz', quizRoutes);
+app.use('/favorites', favoriteRoutes);
+app.use('/blacklist', blacklistRoutes);
+
+app.get('/', (req, res) => {
+  res.render('index');
+});
 
 // 404 fallback:laten staan
 app.use((req, res) => {
