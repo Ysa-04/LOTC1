6c245cd399f053786ac8e5357b757694e8db7745
Britt van de Reijt <britt.1412vandereijt@gmail.com>
2025-05-27 18:39:57 +0000

    extra

--- src/app.ts
+++ src/app.ts
@@ -26,6 +26,10 @@ app.use(cookieParser());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+app.use((req, res, next) => {
+  console.log('➡️ Route aangeroepen:', req.method, req.url);
+  next();
+});
 
 app.use('/login', loginRoutes);
 app.use('/quiz', quizRoutes);
