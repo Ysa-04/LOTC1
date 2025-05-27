1ed6e43d81a7e1d36f9a9e425d590b8f2a15bb8b
Britt van de Reijt <britt.1412vandereijt@gmail.com>
2025-05-27 19:12:16 +0000

    extra

--- src/routes/loginRoutes.ts
+++ src/routes/loginRoutes.ts
@@ -4,6 +4,14 @@ import { login, register } from '../controllers/loginControllers';
 
 const router = express.Router();
 
+router.get('/login', (req, res) => {
+  res.render('login'); // toont login.ejs
+});
+
+router.get('/register', (req, res) => {
+  res.render('register'); // toont login.ejs
+});
+
 router.post('/login', login);
 router.post('/register', register);
 
