6bf2ac34c4810b9ea3344e49ee3feac0b46cd9fc
Ysa-04 <ysaura2903@outlook.com>
2025-05-27 13:29:16 +0000

    database

--- src/models/database.ts
+++ src/models/database.ts
@@ -1,13 +1,27 @@
-import { MongoClient } from 'mongodb';
+import { MongoClient, Collection } from "mongodb";
+import dotenv from "dotenv";
+dotenv.config();
 
-const client = new MongoClient(process.env.MONGO_URI || '');
-let database: any;
+const CONNECTION_STRING = process.env.MONGO_URI || "mongodb://localhost:27017";
+const client = new MongoClient(CONNECTION_STRING);
+const dbName = "lotr";
+let db: any;
+
+export let usersCollection: Collection;
+export let quotesCollection: Collection;
+export let filmsCollection: Collection;
+export let charactersCollection: Collection;
 
 export async function connectToDb() {
   await client.connect();
-  database = client.db('lotr');
+  db = client.db(dbName);
+
+  usersCollection = db.collection("users");
+  quotesCollection = db.collection("quotes");
+  filmsCollection = db.collection("films");
+  charactersCollection = db.collection("characters");
 }
 
-export function db() {
-  return database;
+export function getDb() {
+  return db;
 }
